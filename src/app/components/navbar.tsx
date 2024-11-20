46c31427a773b401d828e570ff22a3d8384df108
mehdinathani <mehdinathani@gmail.com>
2024-11-20 08:11:54 +0500

    add sections

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,13 +1,26 @@
+'use client'
 import Image from "next/image";
+import { useState } from "react";
 
 export default function NavBar() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
-        <nav className="flex justify-between px-4 py-6 items-center bg-white">
+        <nav className="flex justify-between px-4 py-6 items-center bg-white relative">
             {/* menu icon */}
-            <Image className="ml-3 sm:hidden" src="/assets/hamburger.png" alt='Hamburger' width={18.75} height={14.25} />
+            <Image
+                className="ml-3 sm:hidden cursor-pointer"
+                src="/assets/hamburger.png"
+                alt="Hamburger"
+                width={18.75}
+                height={14.25}
+                onClick={() => setMenuOpen(!menuOpen)}
+            />
 
             {/* logo */}
-            <div className="ml-3 text-black font-bold text-[25.2px] leading-[30.24px] font-sans sm:text-[32px] sm:leading-[38.4px]">SHOP.CO</div>
+            <div className="ml-3 text-black font-bold text-[25.2px] leading-[30.24px] font-sans sm:text-[32px] sm:leading-[38.4px]">
+                SHOP.CO
+            </div>
 
             {/* navbar items */}
             <div className="flex">
@@ -18,6 +31,7 @@ export default function NavBar() {
                     <li>Brands</li>
                 </ul>
             </div>
+
             {/* search bar */}
             <div className="hidden sm:flex items-center bg-[#F0F0F0] w-[577px] h-[48px] px-4 gap-4 rounded-[62px]">
                 <Image src="/assets/search_icon.png" alt="Search Icon" width={24} height={24} />
@@ -34,6 +48,18 @@ export default function NavBar() {
                 <Image src="/assets/cart.png" alt="Cart" width={24} height={24} />
                 <Image src="/assets/profile.png" alt="Profile" width={24} height={24} />
             </div>
+
+            {/* Mobile Menu Box */}
+            {menuOpen && (
+                <div className="absolute top-full left-0 w-full bg-white shadow-md z-10 sm:hidden">
+                    <ul className="flex flex-col gap-4 p-4">
+                        <li>Shop &#x25BC;</li>
+                        <li>On Sale</li>
+                        <li>New Arrival</li>
+                        <li>Brands</li>
+                    </ul>
+                </div>
+            )}
         </nav>
     );
 }
